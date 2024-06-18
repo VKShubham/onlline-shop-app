ce712ec16bf1d06f2aa157aee268c32386827544
Shubham Vanani <shubhamvanani@gmail.com>
2024-06-18 19:22:06 +0530

    Auth Routing is Set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path : '/',
+      element : <SignupPage />
+    },
+    {
+      path : '/login',
+      element : <LoginPage />
+    },
+    {
+      path : '/home',
+      element : <HomePage />
+    }
+  ])
+
   return (
-    <>
-      <HomePage />
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
